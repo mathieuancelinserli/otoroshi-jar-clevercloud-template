05ad32c41fb4bb1a817c8da5f173424f3ad8c926
Mathieu ANCELIN <mathieu.ancelin@serli.com>
2018-02-09 17:04:00 +0100

    Update build.sh

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -2,4 +2,4 @@
 
 echo "Run build from CC hooks"
 
-wget -q --show-progress 'https://dl.bintray.com/maif/binaries/otoroshi.jar/1.0.0/otoroshi.jar'
+wget -q --show-progress 'https://dl.bintray.com/maif/binaries/otoroshi.jar/1.0.1/otoroshi.jar'
